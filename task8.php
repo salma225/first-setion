c87046356f29053babcb637336b7fd3dfb7cef1a
salma225 <74434579+salma225@users.noreply.github.com>
2021-09-15 03:47:09 +0200

    Add files via upload

---
+++ task8.php
@@ -0,0 +1,38 @@
+<?php
+
+/*
+doctor
+id  name    email          password     specialization     gender  
+1   d1name  doc1@admin.com   20201      brain and nerves  f
+2   d2name   doc2@admin.com   57569     bones             m
+
+admin
+id    name    email              password
+1     admin1  admin1@admin.com   12345
+2     admin2  admin2@admin.com   891011
+
+patient
+id   name        email           password
+1    p1name   patient1@admin.com   54857
+2    p2name   patient2@admin.com   22154
+
+
+doctor    patient
+1         m
+m          1
+m=>m
+
+
+
+reserves
+id doc.id  pat.id dattime cost
+
+
+
+
+
+*/
+
+
+
+?>
